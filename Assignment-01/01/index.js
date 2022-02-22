06a1be3e0241f2a9fd4d9472898758f109068fd1
abhi616 <ay891241@gmail.com>
2022-02-22 22:58:36 +0530

    Basic Js Assignment

--- Assignment-01/01/index.js
+++ Assignment-01/01/index.js
@@ -3,7 +3,7 @@ let n2 = parseInt(prompt("Enter 2nd Number : "));
 
 let Ans = parseInt(divide(n1,n2));
 
-document.write("\n Division Of "+ n1 + " & " + n2 + " = " + Ans);
+alert("\n Division Of "+ n1 + " & " + n2 + " = " + Ans);
 
 function divide(){   
         return n1/n2;  
